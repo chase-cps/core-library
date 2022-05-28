e7aeba863bae6e25957c17816c7dcaed3cdec601
Michele Lora <michele.lora@univr.it>
2022-05-27 17:27:08 -0700

    Initializaztion: base source code ported from main repository

---
+++ include/chase-core.hh
@@ -0,0 +1,13 @@
+/*
+ * @brief Main include file of the Chase core.
+ * @author Michele Lora.
+ * @date 27-May-2022
+ *
+ * */
+
+#pragma once
+
+#include "chase/representation.hh"
+#include "chase/utilities.hh"
+
+#include <cstdint>
---
+++ include/utilities.hh
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "utilities/IOUtils.hh"
+#include "utilities/UtilityFunctions.hh"
+#include "utilities/LogicNotNormalizationVisitor.hh"
+#include "utilities/VarsCausalityVisitor.hh"
+#include "utilities/LogicIdentificationVisitor.hh"
+#include "GuideVisitor.hh"
+
+// #include "utilities/Copy.hh"
